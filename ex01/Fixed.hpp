2d2ed03355d03f096f2b458e0025cf2238f0a963
Valentin LEPILLER <vlepille@student.42.fr>
2023-09-12 18:49:44 +0200

    Start ex01

---
+++ ex01/Fixed.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+class Fixed
+{
+private:
+	int		_value;
+	static const int	_fractional_bits = 8;
+public:
+	Fixed(); // Default constructor
+	Fixed(const Fixed &src); // Copy constructor
+	Fixed(const int value);
+	Fixed(const float value);
+	Fixed	&operator=(const Fixed &src); // Assignment operator
+
+	~Fixed(); // Destructor
+	int		getRawBits(void) const;
+	void	setRawBits(int const raw);
+	float	toFloat(void) const;
+	int		toInt(void) const;
+};
