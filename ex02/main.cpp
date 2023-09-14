6eadce9ce9be32f5adc2a08f4743bb2dfbb13d68
Valentin LEPILLER <vlepille@student.42.fr>
2023-09-14 18:45:49 +0200

    Do ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "Fixed.hpp"
+
+int main( void ) {
+	Fixed a;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	Fixed const c(b);
+
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+
+	std::cout << b << std::endl;
+
+	std::cout << Fixed::max( a, b ) << std::endl;
+	std::cout << (a < b) << std::endl;
+	std::cout << (a > b) << std::endl;
+	return 0;
+}
