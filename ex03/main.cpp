931c001084b27ebcb98b8765bffb78cb901356fb
Valentin LEPILLER <vlepille@student.42.fr>
2023-09-18 12:44:52 +0200

    Add some tests for inf and Nan float values

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -27,5 +27,37 @@ int main( void ) {
 	std::cout << "bsp(a, b, c, point) = " << bsp(a, b, c, point3) << std::endl;
 	std::cout << "bsp(a, b, c, point) = " << bsp(a, b, c, point4) << std::endl;
 	std::cout << "bsp(a, b, c, point) = " << bsp(a, b, c, point5) << std::endl;
+
+	// inf and NaN tests
+	Point point6(0.0f / 0.0f, 0.0f / 0.0f);
+	Point point7(0.0f / 0.0f, 1.0f / 0.0f);
+	Point point8(0.0f / 0.0f, -1.0f / 0.0f);
+	Point point9(1.0f / 0.0f, 1.0f / 0.0f);
+	Point point10(1.0f / 0.0f, 0.0f / 0.0f);
+	Point point11(1.0f / 0.0f, -1.0f / 0.0f);
+	Point point12(-1.0f / 0.0f, -1.0f / 0.0f);
+	Point point13(-1.0f / 0.0f, 0.0f / 0.0f);
+	Point point14(-1.0f / 0.0f, 1.0f / 0.0f);
+
+	std::cout << "point6 = " << point6 << std::endl;
+	std::cout << "point7 = " << point7 << std::endl;
+	std::cout << "point8 = " << point8 << std::endl;
+	std::cout << "point9 = " << point9 << std::endl;
+	std::cout << "point10 = " << point10 << std::endl;
+	std::cout << "point11 = " << point11 << std::endl;
+	std::cout << "point12 = " << point12 << std::endl;
+	std::cout << "point13 = " << point13 << std::endl;
+	std::cout << "point14 = " << point14 << std::endl;
+	std::cout << "bsp(a, b, c, point) = " << bsp(a, b, c, point6) << std::endl;
+	std::cout << "bsp(a, b, c, point) = " << bsp(a, b, c, point7) << std::endl;
+	std::cout << "bsp(a, b, c, point) = " << bsp(a, b, c, point8) << std::endl;
+	std::cout << "bsp(a, b, c, point) = " << bsp(a, b, c, point9) << std::endl;
+	std::cout << "bsp(a, b, c, point) = " << bsp(a, b, c, point10) << std::endl;
+	std::cout << "bsp(a, b, c, point) = " << bsp(a, b, c, point11) << std::endl;
+	std::cout << "bsp(a, b, c, point) = " << bsp(a, b, c, point12) << std::endl;
+	std::cout << "bsp(a, b, c, point) = " << bsp(a, b, c, point13) << std::endl;
+	std::cout << "bsp(a, b, c, point) = " << bsp(a, b, c, point14) << std::endl;
+	std::cout << "bsp(a, b, c, point) = " << bsp(point9, point12, point6, point) << std::endl;
+	std::cout << "bsp(a, b, c, point) = " << bsp(point9, point12, point6, point14) << std::endl;
 	return 0;
 }
