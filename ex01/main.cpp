aa44893edbb4dc10f7c923cb0a5a156ae71a208d
Valentin LEPILLER <vlepille@student.42.fr>
2023-09-18 12:25:08 +0200

    Add some test for inf and NaN values

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,6 +6,9 @@ int main( void ) {
 	Fixed const b( 10 );
 	Fixed const c( 42.42f );
 	Fixed const d( b );
+	Fixed const e( 0.0f / 0.0f );
+	Fixed const f( 1.0f / 0.0f );
+	Fixed const g( -1.0f / 0.0f );
 
 	a = Fixed( 1234.4321f );
 
@@ -13,11 +16,17 @@ int main( void ) {
 	std::cout << "b is " << b << std::endl;
 	std::cout << "c is " << c << std::endl;
 	std::cout << "d is " << d << std::endl;
+	std::cout << "e is " << e << std::endl;
+	std::cout << "f is " << f << std::endl;
+	std::cout << "g is " << g << std::endl;
 
 	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
 	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
 	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
 	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	std::cout << "e is " << e.toInt() << " as integer" << std::endl;
+	std::cout << "f is " << f.toInt() << " as integer" << std::endl;
+	std::cout << "g is " << g.toInt() << " as integer" << std::endl;
 
 	return 0;
 }
